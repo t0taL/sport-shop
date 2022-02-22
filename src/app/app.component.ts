13fea252fab0ee4cf0a678234c847c9a76ed4c7d
User <vitaly.korneev@testit.software>
2022-02-22 22:24:28 +0300

    added header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sport-shop';
+  public title = 'sdfsdfsdf';
+
+  public array: number[] = [1,2,3,4,5,6,7,8,9];
+
+  public showHiddenElement = false;
 }
